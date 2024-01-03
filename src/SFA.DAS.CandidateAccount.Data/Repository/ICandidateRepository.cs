74c7257e5d43fd674843c3883e8979c70e380975
Peter Williams <peter.david.williams.1991@gmail.com>
2024-01-03 15:38:41 +0000

    Added Add and GetByEmail methods for candidate repo. Added associated unit tests. Get by email happy path test currently failing

---
+++ src/SFA.DAS.CandidateAccount.Data/Repository/ICandidateRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.CandidateAccount.Data.Repository
+{
+    public interface ICandidateRepository
+    {
+        Task Insert(Domain.Candidate.CandidateEntity candidate);
+        Task<Domain.Candidate.CandidateEntity> GetCandidateByEmail(string email);
+    }
+}
