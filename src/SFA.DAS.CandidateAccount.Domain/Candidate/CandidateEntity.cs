766a287c1350afbe00bce5e9242d9fe8508244a5
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-01-25 08:17:20 +0000

    FAI-1099 - Add mapping from domain to entity for candidate

--- src/SFA.DAS.CandidateAccount.Domain/Candidate/CandidateEntity.cs
+++ src/SFA.DAS.CandidateAccount.Domain/Candidate/CandidateEntity.cs
@@ -4,6 +4,23 @@ namespace SFA.DAS.CandidateAccount.Domain.Candidate;
 
 public record CandidateEntity
 {
+    public static implicit operator CandidateEntity(Candidate source)
+    {
+        return new CandidateEntity
+        {
+            Id = source.Id,
+            Email = source.Email,
+            FirstName = source.FirstName,
+            LastName = source.LastName,
+            GovUkIdentifier = source.GovUkIdentifier,
+            DateOfBirth = source.DateOfBirth,
+            CreatedOn = source.CreatedOn,
+            TermsOfUseAcceptedOn = source.TermsOfUseAcceptedOn,
+            MiddleNames = source.MiddleNames,
+            PhoneNumber = source.PhoneNumber,
+            UpdatedOn = source.UpdatedOn
+        };
+    }
     public Guid Id { get; set; }
     public string? FirstName { get; set; }
     public string? MiddleNames { get; set; }
