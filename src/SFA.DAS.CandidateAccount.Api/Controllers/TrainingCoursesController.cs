51281dd7ef0e7a9fd1fd82e967993f9e97674350
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-02-12 10:55:36 +0000

    FAI-1092 add GetTrainingCourse endpoint

--- src/SFA.DAS.CandidateAccount.Api/Controllers/TrainingCoursesController.cs
+++ src/SFA.DAS.CandidateAccount.Api/Controllers/TrainingCoursesController.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.CandidateAccount.Api.ApiResponses;
+using SFA.DAS.CandidateAccount.Application.Application.Queries.GetTrainingCourseItem;
 using SFA.DAS.CandidateAccount.Application.Application.Queries.GetTrainingCourses;
 
 namespace SFA.DAS.CandidateAccount.Api.Controllers;
@@ -26,6 +27,27 @@ public class TrainingCoursesController(IMediator mediator, ILogger<WorkHistoryCo
         catch (Exception e)
         {
             logger.LogError(e, "GetTrainingCourses : An error occurred");
+            return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+        }
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<IActionResult> Get([FromRoute] Guid candidateId, [FromRoute] Guid applicationId, [FromRoute] Guid id)
+    {
+        try
+        {
+            var result = await mediator.Send(new GetTrainingCourseItemQuery
+            {
+                CandidateId = candidateId,
+                ApplicationId = applicationId,
+                Id = id
+            });
+            return Ok((GetTrainingCourseItemApiResponse)result);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "GetTrainingCourseItem : An error occurred");
             return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
         }
     }
--- src/SFA.DAS.CandidateAccount.Data.UnitTests/DatabaseMock/TestAsyncEnumerator.cs
+++ src/SFA.DAS.CandidateAccount.Data.UnitTests/DatabaseMock/TestAsyncEnumerator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SFA.DAS.CandidateAccount.Data.UnitTests.DatabaseMock
+﻿namespace SFA.DAS.CandidateAccount.Data.UnitTests.DatabaseMock
 {
     public class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
     {
