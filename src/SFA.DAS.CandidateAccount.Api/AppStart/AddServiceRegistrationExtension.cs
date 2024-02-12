fa021777be0e70762a8f7fb0267622876797a980
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-02-12 11:58:40 +0000

    FAI-1092 update service registrations

--- src/SFA.DAS.CandidateAccount.Api/AppStart/AddServiceRegistrationExtension.cs
+++ src/SFA.DAS.CandidateAccount.Api/AppStart/AddServiceRegistrationExtension.cs
@@ -1,6 +1,7 @@
 using SFA.DAS.CandidateAccount.Application.Application.Commands.UpsertApplication;
 using SFA.DAS.CandidateAccount.Data.Application;
 using SFA.DAS.CandidateAccount.Data.Candidate;
+using SFA.DAS.CandidateAccount.Data.TrainingCourse;
 using SFA.DAS.CandidateAccount.Data.WorkExperience;
 
 namespace SFA.DAS.CandidateAccount.Api.AppStart;
@@ -12,6 +13,7 @@ public static class AddServiceRegistrationExtension
         services.AddScoped<IApplicationRepository, ApplicationRepository>();
         services.AddScoped<ICandidateRepository, CandidateRepository>();
         services.AddScoped<IWorkHistoryRepository, WorkHistoryRepository>();
+        services.AddScoped<ITrainingCourseRespository, TrainingCourseRespository>();
         services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(typeof(UpsertApplicationCommand).Assembly));
     }
 }
