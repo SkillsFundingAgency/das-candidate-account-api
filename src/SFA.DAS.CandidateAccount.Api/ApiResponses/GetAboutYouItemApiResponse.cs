dda9ecd40923dd56f71ae290dcfd787e1459d092
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-02-23 11:11:36 +0000

    FAI-1057 update GetCandidateSillsAndStrengths api response

--- src/SFA.DAS.CandidateAccount.Api/ApiResponses/GetAboutYouItemApiResponse.cs
+++ src/SFA.DAS.CandidateAccount.Api/ApiResponses/GetAboutYouItemApiResponse.cs
@@ -4,15 +4,28 @@ namespace SFA.DAS.CandidateAccount.Api.ApiResponses;
 
 public class GetAboutYouItemApiResponse
 {
+    public AboutYouItem? AboutYou { get; set; }
+
+    public static implicit operator GetAboutYouItemApiResponse(GetAboutYouItemQueryResult source)
+    {
+        return new GetAboutYouItemApiResponse
+        {
+            AboutYou = source.ApplicationId is null ? null : (AboutYouItem)source
+        };
+    }
+}
+
+public class AboutYouItem
+{
     public string? SkillsAndStrengths { get; set; }
     public string? Improvements { get; set; }
     public string? HobbiesAndInterests { get; set; }
     public string? Support { get; set; }
     public Guid? ApplicationId { get; set; }
 
-    public static implicit operator GetAboutYouItemApiResponse(GetAboutYouItemQueryResult source)
+    public static implicit operator AboutYouItem(GetAboutYouItemQueryResult source)
     {
-        return new GetAboutYouItemApiResponse
+        return new AboutYouItem
         {
             SkillsAndStrengths = source.Strengths,
             Improvements = source.Improvements,
@@ -22,3 +35,4 @@ public class GetAboutYouItemApiResponse
         };
     }
 }
+
