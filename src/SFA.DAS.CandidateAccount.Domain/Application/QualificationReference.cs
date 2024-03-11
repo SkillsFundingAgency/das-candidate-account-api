a7bd351a68d5b4aa2428850a1112e452d3dccffa
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-03-11 19:21:32 +0000

    FAI-1179 - Add mapping from entity to domain

---
+++ src/SFA.DAS.CandidateAccount.Domain/Application/QualificationReference.cs
@@ -0,0 +1,15 @@
+namespace SFA.DAS.CandidateAccount.Domain.Application;
+
+public class QualificationReference
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public static implicit operator QualificationReference(QualificationReferenceEntity source)
+    {
+        return new QualificationReference
+        {
+            Id = source.Id,
+            Name = source.Name
+        };
+    }
+}
--- src/SFA.DAS.CandidateAccount.Domain/Application/QualificationReferenceEntity.cs
+++ src/SFA.DAS.CandidateAccount.Domain/Application/QualificationReferenceEntity.cs
@@ -5,6 +5,6 @@ namespace SFA.DAS.CandidateAccount.Domain.Application;
 public class QualificationReferenceEntity
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public virtual Lazy<Collection<QualificationEntity>> QualificationEntity { get; set; }
 }
