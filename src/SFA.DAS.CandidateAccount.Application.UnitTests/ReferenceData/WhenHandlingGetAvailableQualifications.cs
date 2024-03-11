05e57f9ce8fb6383e7872ae040fc5d1e57677b5b
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-03-11 19:36:33 +0000

    FAI-1179 - Add handler for getting qualification reference data

---
+++ src/SFA.DAS.CandidateAccount.Application.UnitTests/ReferenceData/WhenHandlingGetAvailableQualifications.cs
@@ -0,0 +1,25 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using SFA.DAS.CandidateAccount.Application.ReferenceData.Queries;
+using SFA.DAS.CandidateAccount.Data.ReferenceData;
+using SFA.DAS.CandidateAccount.Domain.Application;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.CandidateAccount.Application.UnitTests.ReferenceData;
+
+public class WhenHandlingGetAvailableQualifications
+{
+    [Test, RecursiveMoqAutoData]
+    public async Task Then_The_Repository_Is_Called_And_Data_Returned(
+        List<QualificationReferenceEntity> data,
+        [Frozen]Mock<IQualificationReferenceRepository> repository,
+        GetAvailableQualificationsQueryHandler handler)
+    {
+        repository.Setup(x => x.GetAll()).ReturnsAsync(data);
+        
+        var actual = await handler.Handle(new GetAvailableQualificationsQuery(), CancellationToken.None);
+
+        actual.QualificationReferences.Should().BeEquivalentTo(data.Select(c => (QualificationReference)c).ToList());
+    }
+}
---
+++ src/SFA.DAS.CandidateAccount.Application/ReferenceData/Queries/GetAvailableQualificationsQuery.cs
@@ -0,0 +1,8 @@
+using MediatR;
+
+namespace SFA.DAS.CandidateAccount.Application.ReferenceData.Queries;
+
+public class GetAvailableQualificationsQuery : IRequest<GetAvailableQualificationsQueryResult>
+{
+    
+}
