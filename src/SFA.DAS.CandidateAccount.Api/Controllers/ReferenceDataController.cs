faf8321f142652a1cc3df157f0ebdc3c30cce26e
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-03-11 20:03:13 +0000

    FAI-1179 - Add controller and action

---
+++ src/SFA.DAS.CandidateAccount.Api/Controllers/ReferenceDataController.cs
@@ -0,0 +1,28 @@
+using System.Net;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.CandidateAccount.Application.ReferenceData.Queries;
+
+namespace SFA.DAS.CandidateAccount.Api.Controllers;
+
+[ApiVersion("1.0")]
+[ApiController]
+[Route("api/[controller]/")]
+public class ReferenceDataController(IMediator mediator, ILogger<ReferenceDataController> logger) : Controller
+{
+    [HttpGet]
+    [Route("qualifications")]
+    public async Task<IActionResult> GetQualifications()
+    {
+        try
+        {
+            var result = await mediator.Send(new GetAvailableQualificationsQuery());
+            return Ok(result.QualificationReferences);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "GetQualifications : An error occurred");
+            return new StatusCodeResult((int) HttpStatusCode.InternalServerError);
+        }
+    }
+}
