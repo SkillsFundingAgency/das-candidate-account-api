21c19629377669343ced84c6e452ea7262b2fd67
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-03-25 12:24:01 +0000

    FAI-1115 add nullables

--- src/SFA.DAS.CandidateAccount.Api/ApiRequests/AddressRequest.cs
+++ src/SFA.DAS.CandidateAccount.Api/ApiRequests/AddressRequest.cs
@@ -4,8 +4,8 @@ public class AddressRequest
 {
     public string Email { get; set; }
     public string AddressLine1 { get; set; }
-    public string AddressLine2 { get; set; }
+    public string? AddressLine2 { get; set; }
     public string AddressLine3 { get; set; }
-    public string AddressLine4 { get; set; }
+    public string? AddressLine4 { get; set; }
     public string Postcode { get; set; }
 }
--- src/SFA.DAS.CandidateAccount.Application/UserAccount/Address/CreateUserAddressCommand.cs
+++ src/SFA.DAS.CandidateAccount.Application/UserAccount/Address/CreateUserAddressCommand.cs
@@ -6,8 +6,8 @@ public class CreateUserAddressCommand : IRequest<CreateUserAddressCommandResult>
     public string GovUkIdentifier { get; set; }
     public string Email { get; set; }
     public string AddressLine1 { get; set; }
-    public string AddressLine2 { get; set; }
+    public string? AddressLine2 { get; set; }
     public string AddressLine3 { get; set; }
-    public string AddressLine4 { get; set; }
+    public string? AddressLine4 { get; set; }
     public string Postcode { get; set; }
 }
