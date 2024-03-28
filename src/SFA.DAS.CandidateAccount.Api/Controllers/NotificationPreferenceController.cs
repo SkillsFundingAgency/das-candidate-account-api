693f1ac53751abfa84f0f2dee83af7a60adbb996
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-03-28 11:22:06 +0000

    FAI-1108 add get candidatePreferences endpoint

---
+++ src/SFA.DAS.CandidateAccount.Api/Controllers/NotificationPreferenceController.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.CandidateAccount.Application.CandidatePreferences.Queries.GetCandidatePreferences;
+
+namespace SFA.DAS.CandidateAccount.Api.Controllers;
+
+[ApiVersion("1.0")]
+[ApiController]
+[Route("api/candidates/{candidateId}/[controller]s/")]
+public class NotificationPreferenceController(IMediator mediator, ILogger<NotificationPreferenceController> logger) : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Get([FromRoute] Guid candidateId)
+    {
+        try
+        {
+            var result = await mediator.Send(new GetCandidatePreferencesQuery() { CandidateId = candidateId });
+            return Ok(result);
+        }
+        catch(Exception e)
+        {
+            logger.LogError(e, "Get notification preferences: An error has occurred");
+            return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+        }
+    }
+}
