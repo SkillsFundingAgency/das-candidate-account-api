487cc1256aee9ca99cb197cfa00bfb2351c7cd3a
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-03-28 11:19:37 +0000

    FAI-1108 update candidatePreferences entity

--- src/SFA.DAS.CandidateAccount.Domain/Candidate/CandidatePreferencesEntity.cs
+++ src/SFA.DAS.CandidateAccount.Domain/Candidate/CandidatePreferencesEntity.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SFA.DAS.CandidateAccount.Domain.Candidate
+﻿namespace SFA.DAS.CandidateAccount.Domain.Candidate
 {
     public class CandidatePreferencesEntity
     {
         public Guid Id { get; set; }
         public Guid CandidateId { get; set; }
         public Guid PreferenceId { get; set; }
-        public required string Status { get; set; }
+        public bool? Status { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime? UpdatedOn { get; set; }
+        public string ContactMethod { get; set; }
     }
 }
