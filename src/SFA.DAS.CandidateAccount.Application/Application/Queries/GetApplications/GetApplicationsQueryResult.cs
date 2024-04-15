77b106347acb4e9f4c5d87c0ec149993da86557d
chrisfoster76 <chrisfoster186@googlemail.com>
2024-04-15 13:07:12 +0100

    Adds endpoint to retrieve applications by candidateId and status

---
+++ src/SFA.DAS.CandidateAccount.Application/Application/Queries/GetApplications/GetApplicationsQueryResult.cs
@@ -0,0 +1,6 @@
+﻿namespace SFA.DAS.CandidateAccount.Application.Application.Queries.GetApplications;
+
+public class GetApplicationsQueryResult
+{
+    public List<Domain.Application.Application> Applications { get; set; } = [];
+}
