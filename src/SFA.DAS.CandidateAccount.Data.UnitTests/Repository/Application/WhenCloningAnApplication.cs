68d59a91e9d813afad865cfc195c4ef539076294
chrisfoster76 <chrisfoster186@googlemail.com>
2024-04-30 14:07:44 +0100

    WIP

---
+++ src/SFA.DAS.CandidateAccount.Data.UnitTests/Repository/Application/WhenCloningAnApplication.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using SFA.DAS.CandidateAccount.Data.Application;
+using SFA.DAS.CandidateAccount.Data.UnitTests.DatabaseMock;
+using SFA.DAS.CandidateAccount.Domain.Application;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.CandidateAccount.Data.UnitTests.Repository.Application
+{
+    public class WhenCloningAnApplication
+    {
+        [Test, RecursiveMoqAutoData]
+        public async Task Then_The_Result_Has_Previous_Answers(
+            ApplicationEntity originalApplication,
+            [Frozen] Mock<ICandidateAccountDataContext> context,
+            ApplicationRepository repository)
+        {
+            context.Setup(x => x.ApplicationEntities)
+                .ReturnsDbSet(new List<ApplicationEntity>{originalApplication});
+
+            var actual = await repository.Clone(originalApplication.Id, true);
+
+            context.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+
+            actual.QualificationEntities.Should().BeEquivalentTo(originalApplication.QualificationEntities);
+            actual.TrainingCourseEntities.Should().BeEquivalentTo(originalApplication.TrainingCourseEntities);
+            actual.WorkHistoryEntities.Should().BeEquivalentTo(originalApplication.WorkHistoryEntities);
+
+            actual.DisabilityConfidenceStatus.Should().Be((short)SectionStatus.PreviousAnswer);
+            actual.JobsStatus.Should().Be((short)SectionStatus.PreviousAnswer);
+            actual.AdditionalQuestion1Status.Should().Be((short)SectionStatus.PreviousAnswer);
+            actual.AdditionalQuestion2Status.Should().Be((short)SectionStatus.PreviousAnswer);
+            actual.InterestsStatus.Should().Be((short)SectionStatus.PreviousAnswer);
+            actual.QualificationsStatus.Should().Be((short)SectionStatus.PreviousAnswer);
+            actual.WorkExperienceStatus.Should().Be((short)SectionStatus.PreviousAnswer);
+            actual.SkillsAndStrengthStatus.Should().Be((short)SectionStatus.PreviousAnswer);
+            actual.TrainingCoursesStatus.Should().Be((short)SectionStatus.PreviousAnswer);
+            actual.InterviewAdjustmentsStatus.Should().Be((short)SectionStatus.PreviousAnswer);
+        }
+    }
+}
