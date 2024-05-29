303de000de54eddd1e43106dcaebadcf7ea234fe
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-05-29 21:57:36 +0100

    FAI-1259 - Add entity to domain model mapping

---
+++ src/SFA.DAS.CandidateAccount.Domain/Candidate/Preference.cs
@@ -0,0 +1,18 @@
+namespace SFA.DAS.CandidateAccount.Domain.Candidate;
+
+public class Preference
+{
+    public Guid PreferenceId { get; set; }
+    public string PreferenceMeaning { get; set; }
+    public string PreferenceHint { get; set; }
+    
+    public static implicit operator Preference(PreferenceEntity source)
+    {
+        return new Preference
+        {
+            PreferenceHint = source.PreferenceHint,
+            PreferenceId = source.PreferenceId,
+            PreferenceMeaning = source.PreferenceMeaning
+        };
+    }
+}
--- src/SFA.DAS.CandidateAccount.Domain/Candidate/PreferenceEntity.cs
+++ src/SFA.DAS.CandidateAccount.Domain/Candidate/PreferenceEntity.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SFA.DAS.CandidateAccount.Domain.Candidate
+namespace SFA.DAS.CandidateAccount.Domain.Candidate;
+public class PreferenceEntity
 {
-    public class PreferenceEntity
-    {
-        public Guid PreferenceId { get; set; }
-        public string PreferenceMeaning { get; set; }
-        public string PreferenceHint { get; set; }
-    }
+    public Guid PreferenceId { get; set; }
+    public string PreferenceMeaning { get; set; }
+    public string PreferenceHint { get; set; }
 }
