b8fe9addcc67c47baec04f1053d050ca3239c9b7
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-07-03 18:46:25 +0100

    FAI-1206 - Add new handler for getting user by migrated candidate id

---
+++ src/SFA.DAS.CandidateAccount.Application/Candidate/Queries/GetCandidateByMigratedId/GetCandidateByMigratedIdQueryResult.cs
@@ -0,0 +1,6 @@
+namespace SFA.DAS.CandidateAccount.Application.Candidate.Queries.GetCandidateByMigratedId;
+
+public class GetCandidateByMigratedIdQueryResult
+{
+    public Domain.Candidate.Candidate? Candidate { get; set; }
+}
