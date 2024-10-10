ee110498a3a0047bae078c7fd505f8fe2a423fa4
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-10-10 09:58:34 +0100

    Reverting the changes

--- src/SFA.DAS.CandidateAccount.Data/Candidate/CandidateRepository.cs
+++ src/SFA.DAS.CandidateAccount.Data/Candidate/CandidateRepository.cs
@@ -19,8 +19,7 @@ public class CandidateRepository(ICandidateAccountDataContext dataContext) : ICa
         var existingCandidate = await dataContext
             .CandidateEntities
             .FirstOrDefaultAsync(c => 
-                c.GovUkIdentifier == candidate.GovUkIdentifier || 
-                c.Email == candidate.Email);
+                c.GovUkIdentifier == candidate.GovUkIdentifier);
 
         if (existingCandidate != null) return new Tuple<CandidateEntity, bool>(existingCandidate, false);
 
