40fe7e426962d8aff62f3c4120e494912107b100
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-10-30 15:03:23 +0000

    DASD-12093 - Change connection string format for SQL

--- src/SFA.DAS.CandidateAccount.Api/AppStart/DatabaseExtensions.cs
+++ src/SFA.DAS.CandidateAccount.Api/AppStart/DatabaseExtensions.cs
@@ -10,28 +10,16 @@ public static class DatabaseExtensions
     public static void AddDatabaseRegistration(this IServiceCollection services, CandidateAccountConfiguration config, string? environmentName)
     {
         services.AddHttpContextAccessor();
-        if (environmentName.Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
+        if (environmentName!.Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
         {
             services.AddDbContext<CandidateAccountDataContext>(options => options.UseInMemoryDatabase("SFA.DAS.CandidateAccount"), ServiceLifetime.Transient);
-        }
-        else if (environmentName.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase))
-        {
-            services.AddDbContext<CandidateAccountDataContext>(options=>options.UseSqlServer(config.ConnectionString),ServiceLifetime.Transient);
         }
         else
         {
             services.AddDbContext<CandidateAccountDataContext>(ServiceLifetime.Transient);    
         }
-            
-        services.AddSingleton(new EnvironmentConfiguration(environmentName));
-
+        
         services.AddScoped<ICandidateAccountDataContext, CandidateAccountDataContext>(provider => provider.GetService<CandidateAccountDataContext>()!);
         services.AddScoped(provider => new Lazy<CandidateAccountDataContext>(provider.GetService<CandidateAccountDataContext>()!));
-        services.AddSingleton(new ChainedTokenCredential(
-            new ManagedIdentityCredential(),
-            new AzureCliCredential(),
-            new VisualStudioCodeCredential(),
-            new VisualStudioCredential())
-        );
     }
 }
